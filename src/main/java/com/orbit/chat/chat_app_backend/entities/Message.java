3597a840152923ffb186da392478775bf04b7111
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2025-09-30 02:32:22 +0530

    Start of chat application

---
+++ src/main/java/com/orbit/chat/chat_app_backend/entities/Message.java
@@ -0,0 +1,26 @@
+package com.orbit.chat.chat_app_backend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Message {
+
+    private String sender;
+    private String content;
+    private LocalDateTime timeStamp;
+
+
+    public Message(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+        this.timeStamp = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/orbit/chat/chat_app_backend/entities/Room.java
@@ -0,0 +1,26 @@
+package com.orbit.chat.chat_app_backend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collation = "rooms")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Room  {
+    @Id
+    private String id; //mongodb unique identifier
+    private String roomId;
+
+    private List<Message> messages = new ArrayList<>();
+
+
+}
