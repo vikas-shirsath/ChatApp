a62f5b56f4de4a2becb22fda55c8f9ff014b20da
vikas-shirsath <vikasshirsath04@gmail.com>
2025-12-23 14:15:41 +0530

    fixing imports

--- src/main/java/com/orbit/chat/chat_app_backend/controller/ChatController.java
+++ src/main/java/com/orbit/chat/chat_app_backend/controller/ChatController.java
@@ -1,18 +1,14 @@
 package com.orbit.chat.chat_app_backend.controller;
 
 import com.orbit.chat.chat_app_backend.entities.Message;
-import com.orbit.chat.chat_app_backend.entities.Room;
 import com.orbit.chat.chat_app_backend.payload.MessageRequest;
-import com.orbit.chat.chat_app_backend.repo.RoomRepositories;
 import com.orbit.chat.chat_app_backend.service.ChatService;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping
--- src/main/java/com/orbit/chat/chat_app_backend/service/RoomService.java
+++ src/main/java/com/orbit/chat/chat_app_backend/service/RoomService.java
@@ -3,8 +3,6 @@ package com.orbit.chat.chat_app_backend.service;
 import com.orbit.chat.chat_app_backend.entities.Message;
 import com.orbit.chat.chat_app_backend.entities.Room;
 import com.orbit.chat.chat_app_backend.repo.RoomRepositories;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
